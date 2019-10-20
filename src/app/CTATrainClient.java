3a75a3ac054a9fb88449dca58c8e1f2041c810c5
lmatar <lmatar@hawk.iit.edu>
2019-10-20 11:25:41 -0500

    hi

--- src/app/CTATrainClient.java
+++ src/app/CTATrainClient.java
@@ -40,7 +40,7 @@ public class CTATrainClient {
 				// data from db
 				dbs.setTrainID(train.getID());
 				for (TrainCars car : train.getCars()) {
-					TimeUnit.MILLISECONDS.sleep(200);
+					TimeUnit.MILLISECONDS.sleep(100);
 					car.setWeight();
 					dbs.pushCar(car.getCarID(), car.seatRange());
 				}
